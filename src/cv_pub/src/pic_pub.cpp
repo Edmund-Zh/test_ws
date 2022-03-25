79e05fb2bc83b7f8d763b7b4031b8fbdb9e056ca
Edmund-Zh <834749569@qq.com>
2022-03-25 22:10:48 +0800

    修复长宽比例

--- src/cv_pub/src/pic_pub.cpp
+++ src/cv_pub/src/pic_pub.cpp
@@ -11,7 +11,7 @@ int main(int argc, char** argv)
   image_transport::Publisher pub = it.advertise("camera/image", 1);
  
   //cv::Mat image = cv::imread(argv[1], CV_LOAD_IMAGE_COLOR);
-  cv::Mat image = cv::imread("/home/edmund/test_ws/src/cv_pub/map/a_map1.bmp", CV_LOAD_IMAGE_COLOR);
+  cv::Mat image = cv::imread("/home/edmund/test_ws/src/cv_pub/map/a_map8.bmp", CV_LOAD_IMAGE_COLOR);
   sensor_msgs::ImagePtr msg = cv_bridge::CvImage(std_msgs::Header(), "bgr8", image).toImageMsg();
  
   ros::Rate loop_rate(5);
