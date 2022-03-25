3c63b9e84ddcf7dcf5c3b266144925c68987160a
Edmund-Zh <834749569@qq.com>
2022-03-25 23:16:40 +0800

    完成话题重映射至random_forest，并且通过了map_recorder测试，且可在launch文件中更改图片路径

--- src/cv_pub/src/pic_pub.cpp
+++ src/cv_pub/src/pic_pub.cpp
@@ -2,6 +2,9 @@
 #include <image_transport/image_transport.h>
 #include <opencv2/highgui/highgui.hpp>
 #include <cv_bridge/cv_bridge.h>
+using namespace std;
+
+string _img_path;
  
 int main(int argc, char** argv)
 {
@@ -11,7 +14,18 @@ int main(int argc, char** argv)
   image_transport::Publisher pub = it.advertise("camera/image", 1);
  
   //cv::Mat image = cv::imread(argv[1], CV_LOAD_IMAGE_COLOR);
-  cv::Mat image = cv::imread("/home/edmund/test_ws/src/cv_pub/map/a_map8.bmp", CV_LOAD_IMAGE_COLOR);
+  
+  // //测试参数
+  // bool ifparam5 = nh.hasParam("pic_pub/PATH");
+  // if(ifparam5) 
+	// 	ROS_INFO("PATH exists");
+	// else
+	// 	ROS_INFO("PATH doesn't exist");
+
+  nh.param<std::string>("pic_pub/PATH", _img_path, "/home/edmund/test_ws/src/cv_pub/map/a_map1.bmp");
+
+  cv::Mat image = cv::imread(_img_path, CV_LOAD_IMAGE_COLOR);
+
   sensor_msgs::ImagePtr msg = cv_bridge::CvImage(std_msgs::Header(), "bgr8", image).toImageMsg();
  
   ros::Rate loop_rate(5);
